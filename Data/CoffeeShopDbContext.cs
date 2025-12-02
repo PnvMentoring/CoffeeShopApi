4e3d830a8fcb2800d2b720d97bc0d07ce1a3f7d7
huybmt <levanhuy06042003@gmail.com>
2025-12-02 19:53:51 +0700

    10 | setup entity framework core and mysql database

---
+++ Data/CoffeeShopDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CoffeeShopApi.Data;
+
+public class CoffeeShopDbContext : DbContext
+{
+    public CoffeeShopDbContext(DbContextOptions<CoffeeShopDbContext> options)
+        : base(options)
+    {
+    }
+    
+}
--- Program.cs
+++ Program.cs
@@ -1,11 +1,16 @@
+using CoffeeShopApi.Data;
+using Microsoft.EntityFrameworkCore;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
-
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddControllers();
+builder.Services.AddDbContext<CoffeeShopDbContext>(options =>
+    options.UseMySql(connectionString, 
+        ServerVersion.AutoDetect(connectionString)));
 
 var app = builder.Build();
 
