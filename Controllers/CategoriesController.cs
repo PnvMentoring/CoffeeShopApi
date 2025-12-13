5b1cc5f2b22f637797b0b72817e156b8727f0d0a
huybmt <levanhuy06042003@gmail.com>
2025-12-13 20:44:22 +0700

    17 | create add category api

--- Controllers/CategoriesController.cs
+++ Controllers/CategoriesController.cs
@@ -1,4 +1,5 @@
 ﻿// CategoriesController.cs
+
 using CoffeeShopApi.Database;
 using CoffeeShopApi.Dtos.Request;
 using CoffeeShopApi.Dtos.Response;
@@ -10,10 +11,18 @@ namespace CoffeeShopApi.Controllers;
 
 public class CategoriesController(CoffeeShopDbContext dbContext) : BaseController
 {
-    [HttpPost("add")]
+    [HttpPost]
     public async Task<ActionResult<AddCategoryResponse>> AddCategory(
         [FromBody] AddCategoryRequest request)
     {
+        var existingCategory = await dbContext.Categories
+            .FirstOrDefaultAsync(c => c.Name == request.Name);
+
+        if (existingCategory != null)
+        {
+            return BadRequest(new { message = "Category name already exists" });
+        }
+
         var category = new Category
         {
             Name = request.Name
