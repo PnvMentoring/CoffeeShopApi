df2a9752e16c7bafe750fac3c585fcde8bf4aa74
huybmt <110990408+itvanhuy@users.noreply.github.com>
2025-12-14 20:25:05 +0700

    17 | create add category api (#18)
    * 17 | create add category api
    
    * 17 | create add category api

---
+++ Controllers/CategoriesController.cs
@@ -0,0 +1,45 @@
+﻿// CategoriesController.cs
+
+using CoffeeShopApi.Database;
+using CoffeeShopApi.Dtos.Request;
+using CoffeeShopApi.Dtos.Response;
+using CoffeeShopApi.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoffeeShopApi.Controllers;
+
+public class CategoriesController(CoffeeShopDbContext dbContext) : BaseController
+{
+    [HttpPost]
+    public async Task<ActionResult<AddCategoryResponse>> AddCategory(
+        [FromBody] AddCategoryRequest request)
+    {
+        var existingCategory = await dbContext.Categories
+            .FirstOrDefaultAsync(c => c.Name == request.Name);
+
+        if (existingCategory != null)
+        {
+            return BadRequest(new { message = "Category name already exists" });
+        }
+
+        var category = new Category
+        {
+            Name = request.Name
+        };
+
+        dbContext.Categories.Add(category);
+        await dbContext.SaveChangesAsync();
+
+        return Created("",
+            new
+            {
+                message = "Add Category successfully",
+                category = new AddCategoryResponse
+                {
+                    Id = category.Id,
+                    Name = category.Name
+                }
+            });
+    }
+}
---
+++ Dtos/Response/AddCategoryResponse.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CoffeeShopApi.Dtos.Response;
+
+public class AddCategoryResponse
+{
+    public required string Id { get; init; }
+    
+    public required string Name { get; init; }
+}
